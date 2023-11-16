0abc65aef3b50e78a7231f1f0fbe5dad2844fa21
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-11-16 12:51:05 +0100

    calcolo punteggio

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		ArrayList<String> carte=new ArrayList<>();		//elenco di tutte le carte
 		HashSet<Integer> estratte=new HashSet<>();		//elenco delle posizioni estratte
 		String[] semi= {"denari", "bastoni", "coppe", "spade"};
-		
+		float punteggio=0;
 		
 		Random r=new Random();
 		Scanner sc=new Scanner(System.in);
@@ -23,11 +23,35 @@ public class Main {
 			
 		//estrazione di una carta
 		int pos;
+		String risposta;
 		do {
-			pos=r.nextInt(40);		//da 0 a 39
-		} while (estratte.contains(pos));	//se l'ho già estratta torno indietro
-		estratte.add(pos);		//non può essere più estratto
-		System.out.println("E' uscito: "+carte.get(pos));
+			do {
+				pos=r.nextInt(40);		//da 0 a 39
+			} while (estratte.contains(pos));	//se l'ho già estratta torno indietro
+			estratte.add(pos);		//non può essere più estratto
+			System.out.println("E' uscito: "+carte.get(pos));
+			if(pos>=0 && pos<=6)			//denari
+				punteggio+=pos+1;
+			else if(pos>=7 && pos<=9)
+				punteggio+=0.5f;
+			else if(pos>=10 && pos<=16)		//bastoni
+				punteggio+=pos+1-10; //-9
+			else if(pos>=17 && pos<=19)
+				punteggio+=0.5f;
+			else if(pos>=20 && pos<=26)		//coppe
+				punteggio+=pos+1-20; //-19
+			else if(pos>=27 && pos<=29)
+				punteggio+=0.5f;
+			else if(pos>=30 && pos<=36)		//spade
+				punteggio+=pos+1-30; //-29
+			else if(pos>=37 && pos<=39)
+				punteggio+=0.5f;
+			
+			System.out.println("Punteggio: "+punteggio);
+			System.out.println("\nVuoi continuare (s/n)");
+			risposta=sc.nextLine();
+		} while(risposta.equals("s"));
+		
 	}
 
 }
