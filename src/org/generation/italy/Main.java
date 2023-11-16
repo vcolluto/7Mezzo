268ebb26c3bcc1c9a529a846132967598f1e1931
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-11-16 12:32:55 +0100

    generazione delle carte ottimizzata

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -10,24 +10,18 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<String> carte=new ArrayList<>();		//elenco di tutte le carte
 		HashSet<Integer> estratte=new HashSet<>();		//elenco delle posizioni estratte
+		String[] semi= {"denari", "bastoni", "coppe", "spade"};
+		
+		
 		Random r=new Random();
 		Scanner sc=new Scanner(System.in);
 		
-		//genero le carte di denari			(posizioni da 0..9)
-		for (int i=1;i<=10;i++)
-			carte.add(i+" di denari");
+		//genero le carte 
+		for (int s=0;s<4;s++)
+			for (int i=1;i<=10;i++)
+				carte.add(i+" di "+semi[s]);
 			
-		//genero le carte di bastoni		(posizioni da 10..19)
-		for (int i=1;i<=10;i++)
-			carte.add(i+" di bastoni");
 		
-		//genero le carte di coppe			(posizioni da 20..29)
-		for (int i=1;i<=10;i++)
-			carte.add(i+" di coppe");
-		
-		//genero le carte di spade			(posizioni da 30..39)
-		for (int i=1;i<=10;i++)
-			carte.add(i+" di spade");
 
 	}
 
